1877d7dfc16149864ba82b516fe75ad8e53db2f3
sardorbek-sf <sardor4ik2013@gmail.com>
2021-05-15 02:46:52 -0700

    game created

---
+++ src/components/Board/Board.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Square from '../Square/Square.js';
+
+export default function Board(props) {
+    const renderSquare = (e) => {
+        return <Square value={props.squares[e]}
+            onClick={() => props.onClick(e)}>
+        </Square>
+    }
+
+    return (
+        <div>
+            <div className="row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    )
+}
